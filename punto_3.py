b870a5dbb874e0fc568e7ab1b70899e9b5d82587
sami3000 <cordobamolinasamira@gmail.com>
2021-10-13 17:06:29 -0300

    punto 3 script copiado

---
+++ punto_3.py
@@ -0,0 +1,14 @@
+
+mi_variable = 4
+
+if mi_variable == 0:
+          print("Ha ingresado a la primera validación.")
+
+elif mi_variable > 3:
+          print("Ha ingresado a la segunda validación.")
+
+elif mi_variable > 6:
+          print("Ha ingresado a la tercer validación.")
+
+else: 
+          print("No hay coincidencias.")
