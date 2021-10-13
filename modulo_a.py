64ba7e37f24d69e8bcf187a686b809853e81734b
sami3000 <cordobamolinasamira@gmail.com>
2021-10-13 20:01:28 -0300

    modulos

---
+++ modulo_a.py
@@ -0,0 +1,9 @@
+# Inicio del modulo modulo_a.py
+def hola():
+        print("Hello world!!!")
+
+
+if __name__ == "__main__":
+        hola()
+
+    # fin del modulo_a
---
+++ modulo_b.py
@@ -0,0 +1,1 @@
+from modulo_a import hola
